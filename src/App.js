76da677f95b7fcd612379f57b6525eb45c18e29f
Naveenchand06 <naveenchand0606@gmail.com>
2022-05-12 11:06:20 +0530

    map and tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Map from "react-map-gl";
+
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1IjoibmF2ZWVuY2hhbmQwNiIsImEiOiJjbDJqM243dzgwdHo2M2l0cDk2N3U5cjJoIn0.ohlcu623axJJrlWECX17_Q";
+
+const style = {
+  wrapper: `flex-1 h-full w-full`,
+  main: `h-screen w-screen m-auto`,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>DecenRide</h1>
+      <div className={style.main}>
+        <Map
+          mapboxAccessToken="pk.eyJ1IjoibmF2ZWVuY2hhbmQwNiIsImEiOiJjbDJqM243dzgwdHo2M2l0cDk2N3U5cjJoIn0.ohlcu623axJJrlWECX17_Q"
+          initialViewState={{
+            longitude: 48.121,
+            latitude: 9.141,
+            zoom: 3.5,
+          }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
+      </div>
+    </>
   );
 }
-
 export default App;
