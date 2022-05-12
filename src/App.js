9c44c3a543f28ca0ee145b08cd9cbd950a9595ce
Naveenchand06 <naveenchand0606@gmail.com>
2022-05-12 11:23:09 +0530

    map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Map from "react-map-gl";
 
-const MAPBOX_ACCESS_TOKEN =
-  "pk.eyJ1IjoibmF2ZWVuY2hhbmQwNiIsImEiOiJjbDJqM243dzgwdHo2M2l0cDk2N3U5cjJoIn0.ohlcu623axJJrlWECX17_Q";
-
 const style = {
   wrapper: `flex-1 h-full w-full`,
   main: `h-screen w-screen m-auto`,
@@ -15,7 +12,7 @@ function App() {
       <h1>DecenRide</h1>
       <div className={style.main}>
         <Map
-          mapboxAccessToken="pk.eyJ1IjoibmF2ZWVuY2hhbmQwNiIsImEiOiJjbDJqM243dzgwdHo2M2l0cDk2N3U5cjJoIn0.ohlcu623axJJrlWECX17_Q"
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
           initialViewState={{
             longitude: 48.121,
             latitude: 9.141,
